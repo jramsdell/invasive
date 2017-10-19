5a948c340442d74c2b40a5e8ab338d6b7bab1e30
Bence Cserna <bence@cserna.net>
2017-10-19 16:18:32 -0400

    Create maxent_recover.R

---
+++ scripts/R/maxent_recover.R
@@ -0,0 +1,295 @@
+source("includes.R")
+
+# lambdas <- as.matrix(lambdas)
+#' return q_lambda
+#' @param X matrix of feature values
+#' @param lambda the feature weight vector
+calcQL <- function(X, lambda) {
+  nom <- exp((X %*% lambda))
+  Z <- sum(nom)
+  return(nom / Z)
+}
+
+#' return weightd expected value
+#' @param dist the probability distribution
+#' @param fi feature values
+calcWeightExp <- function(dist, fi) {
+  return (as.vector(dist) %*% as.vector(fi))
+}
+
+#' calculate weight increases
+#' @param q_l q lambda distribution
+#' @param p_t the empirical distribution
+#' @param fi the feature values
+calcAlpha <- function(q_l, p_t, fi) {
+  a <- calcWeightExp(p_t, fi)
+  b <- calcWeightExp(q_l, fi)
+  re <- KL.plugin(c(a, 1 - a), c(b, 1 - b))
+  alpha_1 = log (((a - 0.1) * (1 - b)) / ((1 - a + 0.1) * b))
+  alpha_2 = log (((a + 0.1) * (1 - b)) / ((1 - a - 0.1) * b))
+  alpha_ = log (((a) * (1 - b)) / ((1 - a) * b))
+  ret <- c(alpha_1, alpha_2, alpha_)
+  return (ret)
+}
+
+#' calculate weights for maxent
+#' @param newX data samples
+#' @param pi_tilde the empirical distribution to approximate
+#' @return the generated lambdas, the generated distribution, and the kl divergence
+calcMaxent <- function(newX, pi_base, pi_tilde){
+  lambdas = vector(mode = 'numeric', ncol(newX))
+  rounds <- 0
+  re_prev <- Inf
+  re <- 0
+  n_features <- ncol(newX)
+  converged <- FALSE
+  original_log_loss <-
+    calcWeightExp(pi_tilde, -log(calcQL(newX, lambdas)))
+  original_QL <- calcQL(newX, lambdas)
+  step = 0.1
+  while (!converged && (rounds < 1000)) {
+    min_idx <- NaN
+    min_a <- NaN
+    min_re <- Inf
+    
+    changes = c()
+    changes_idx = c()
+    changes_step = c()
+    QL <- calcQL(newX, lambdas)
+    log_loss <- calcWeightExp(pi_tilde, -log(QL))
+    starting_log_loss <- log_loss
+    for (f in 1:n_features) {
+      i = 0
+      while (1) {
+        # cat(sprintf("Round #%d \tFeature #%d \t i=%d\n", rounds, f, i))
+        temp_lambdas <- lambdas
+        # ret <- calcAlpha(QL, pi_tilde, newX[, f])
+        delta = step
+        temp_lambdas[f] <- temp_lambdas[f] - (2 ^ i) * delta
+        QL_ <- calcQL(newX, temp_lambdas)
+        log_loss_ <- calcWeightExp(pi_tilde, -log(QL_))
+        if (log_loss_ < log_loss) {
+          
+          lambdas <- temp_lambdas
+          # cat(" \tUpdating\n")
+          
+          if (abs(log_loss - log_loss_) < 0.000001) {
+            # cat("Converged\n")
+            converged = TRUE
+            log_loss <- log_loss_
+            break
+          } else{
+            log_loss <- log_loss_
+            break
+          }
+        } else{
+          temp_lambdas <- lambdas
+          delta = -step
+          temp_lambdas[f] <- temp_lambdas[f] - (2 ^ i) * delta
+          QL_ <- calcQL(newX, temp_lambdas)
+          log_loss_ <- calcWeightExp(pi_tilde, -log(QL_))
+          if (log_loss_ < log_loss) {
+            lambdas <- temp_lambdas
+            
+            if (abs(log_loss - log_loss_) < 0.000001) {
+              converged = TRUE
+              log_loss <- log_loss_
+              break
+            } else{
+              log_loss <- log_loss_
+              break
+            }
+          } else{
+            break
+          }
+          i <- i + 1
+        }
+      }
+    }
+    if (abs(log_loss - starting_log_loss) < 0.00001) {
+      converged = TRUE
+    }
+    rounds <- rounds + 1
+  }
+  
+  nominator <- exp((newX %*% lambdas))
+  Z <- sum(nominator)
+  pi_hat <- nominator / Z
+  kl <- KL.plugin(pi_base, pi_hat)
+  retList = list("lambdas"=lambdas, "pi_hat"=pi_hat, "kl"=kl)
+  return(retList)
+}
+# Load a raster so cells are available
+raster <- raster("./bio7_13.tif")
+
+# APPEND A RESPONSE COLUMN TO THE POPULATION DATA
+glossy_data_frame_glm$y <- 1
+
+# nh_data_frame_glm <-
+#   nh_data_frame_glm[sample(nrow(nh_data_frame_glm),
+#                            nrow(glossy_data_frame_glm),
+#                            replace = FALSE), ]
+nh_data_frame_glm$y <- 0
+
+#nh_data_frame_glm = rbind(nh_data_frame_glm, glossy_data_frame_glm)
+
+#X <<- nh_data_frame_glm
+X <<- glossy_data_frame_glm
+
+n_features <- ncol(X) - 1
+#  X dataset (dataframe)
+# row is a data point
+
+n_samples = nrow(X)
+n_samples_pos = sum(X$y)
+
+ALREADY_FOUND = FALSE
+if(ALREADY_FOUND == FALSE){
+  # group by cells but it might not be necessary
+  for (i in 1:nrow(X)) {
+    X$cell[i] <- cellFromXY(raster, cbind(X$lon[i], X$lat[i]))
+  }
+  
+  X <- group_by(X, cell) %>%
+    summarize(
+      # prob = sum(y) / n_samples_pos,
+      lon = mean(lon),
+      lat = mean(lat),
+      bio1 = mean(bio1),
+      bio2 = mean(bio2),
+      bio3 = mean(bio3),
+      bio4 = mean(bio4),
+      bio5 = mean(bio5),
+      bio6 = mean(bio6),
+      bio8 = mean(bio8),
+      bio9 = mean(bio9),
+      bio10 = mean(bio10),
+      bio11 = mean(bio11),
+      bio12 = mean(bio12),
+      bio13 = mean(bio13),
+      bio14 = mean(bio14),
+      bio15 = mean(bio15),
+      bio16 = mean(bio16),
+      bio17 = mean(bio17),
+      bio18 = mean(bio18),
+      bio19 = mean(bio19)
+    )
+  saveRDS(X, "./data/dataWithCells.rds")
+}else{
+  X <<- readRDS("./data/dataWithCells.rds")
+}
+
+# X$cell <- NULL
+X$lon <- NULL
+X$lat <- NULL
+
+cells_array <- X$cell
+X$cell <- NULL
+
+lc <- findLinearCombos(X)
+X[lc$remove] <- NULL
+
+XX<-X
+
+initial_pop = floor(n_samples/5)
+num_steps = 20
+n_iter = 20
+step_size = floor(initial_pop/num_steps)
+step = step_size
+sample_num <- array(0, num_steps)
+kl_divergence <- array(0, num_steps)
+kl_divergence_com <- array(0, num_steps)
+kl_idx <- 1
+for(i in 1:num_steps){
+  sample_num[i] <- step
+  step <- step + step_size
+}
+
+n_features <- ncol(XX)
+
+for(multi_samp in sample_num) {
+  weight_df = data.frame(matrix(ncol = 5, nrow = n_features))
+  dist_df = data.frame(matrix(ncol = 4, nrow = initial_pop))
+  zero <- vector(mode = "numeric", length =  length(multi_samp))
+  for (it in 1:n_iter) {
+    cat(sprintf("KL idx = %d, %d samples, iteration %d \n", kl_idx, sample_num[kl_idx], it))
+    X <- XX[sample(1:nrow(X), initial_pop, replace = TRUE), ]
+    
+    # # normalize, but skip the probablity
+    X <- sapply(X[, 1:ncol(X)], rescale, to = c(0, 1))
+    
+    # assertthat::are_equal(1, sum(prob))
+    X <- as.matrix(X)
+    
+    # create random weights
+    lambdas_base = as.vector(runif(n_features))
+    nominator <- exp((X %*% lambdas_base))
+    Z <- sum(nominator)
+    # create π
+    pi_base <- nominator / Z
+    
+    res <- rmultinom(1, size = multi_samp, prob = pi_base)
+    
+    newX <- X
+    pi_tilde = vector(mode = 'numeric', initial_pop)
+    
+    j = 1
+    for (i in 1:initial_pop) {
+      if (res[i] > 0) {
+        pi_tilde[i] = res[i] / multi_samp
+      } else{
+        pi_tilde[i] = 0.0
+      }
+    }
+    ret <- calcMaxent(newX, pi_base, pi_tilde)
+    kl_divergence[kl_idx] <- kl_divergence[kl_idx] + ret$kl
+    
+    #we now use the derived weights as the generating distribution
+    lambdas_base = ret$lambdas
+    nominator <- exp((X %*% lambdas_base))
+    Z <- sum(nominator)
+    # create π
+    pi_base <- nominator / Z
+    
+    res <- rmultinom(1, size = multi_samp, prob = pi_base)
+    
+    newX <- X
+    pi_tilde = vector(mode = 'numeric', initial_pop)
+    
+    j = 1
+    for (i in 1:initial_pop) {
+      if (res[i] > 0) {
+        pi_tilde[i] = res[i] / multi_samp
+      } else{
+        pi_tilde[i] = 0.0
+      }
+    }
+    ret <- calcMaxent(newX, pi_base, pi_tilde)
+    kl_divergence_com[kl_idx] <- kl_divergence[kl_idx] + ret$kl
+  }
+  kl_divergence_com[kl_idx] <- kl_divergence[kl_idx]/n_iter
+  kl_idx <- kl_idx + 1 
+  
+}
+filename_kl <- paste("KL_Divergence_", format(Sys.Date(), "%m_%d_%y_"), initial_pop,"_", length(kl_divergence), ".pdf", sep="")
+kl_dataframe <- data.frame(x=1:length(kl_divergence)*step_size*initial_pop, kl = kl_divergence)
+
+#kl <- 
+ggplot(kl_dataframe) +
+  geom_line(aes(x=x, y=kl ,color='KL Divergence'))  +
+  geom_point(aes(x = x, y = kl, color = 'KL Divergence')) +
+  xlab("Number of samples") +
+  scale_y_continuous("KL(π, ^π)") +
+  labs(title = "KL Divergence between π, ^π") +
+  theme(plot.title = element_text(hjust = 0.5))
+  
+  #ggplot(kl_com_dataframe)+
+  #  geom_line(aes(x=x, y=kl ,color='KL Divergence'))  +
+  #  geom_point(aes(x = x, y = kl, color = 'KL Divergence')) +
+  #  xlab("Number of samples") +
+  #  scale_y_continuous("KL(π, ^π)") +
+  #  labs(title = "KL Divergence between π, ^π") +
+  #  theme(plot.title = element_text(hjust = 0.5))
+
+#print(kl)
+#ggsave(filename_kl)
